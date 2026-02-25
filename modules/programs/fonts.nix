f1da555663cd0ee04e39b7b06b69ef79915f3b87
Maekilae <marcus@emph.dev>
2026-02-25 22:03:14 +0100

    Create fonts.nix

---
+++ modules/programs/fonts.nix
@@ -0,0 +1,61 @@
+{
+  flake.modules.nixos.fonts =
+    {
+      pkgs,
+      ...
+    }:
+    {
+      fonts = {
+        enableDefaultPackages = true;
+        packages = with pkgs; [
+          # Provides Helvetica and Mac-like metrics alternatives
+          liberation_ttf
+          helvetica-neue-lt-std
+
+          # Modern UI & Web Fonts
+          inter
+          roboto
+          open-sans
+          lato
+          ubuntu_font_family
+          cantarell-fonts
+
+          # Internationalization & Emojis
+          noto-fonts
+          noto-fonts-cjk-sans
+          noto-fonts-emoji
+
+          # Nerd Fonts
+          nerd-fonts.fira-code
+          nerd-fonts.droid-sans-mono
+          nerd-fonts.jetbrains-mono
+
+          ubuntu-sans
+          cm_unicode
+          corefonts
+          unifont
+        ];
+
+        fontconfig.defaultFonts = {
+          sansSerif = [
+            "Inter"
+            "Roboto"
+            "Arial"
+            "Noto Sans"
+          ];
+          serif = [
+            "Georgia"
+            "Times New Roman"
+            "Noto Serif"
+          ];
+          monospace = [
+            "JetBrains Mono"
+            "Fira Code"
+            "Courier New"
+          ];
+          emoji = [ "Noto Color Emoji" ];
+        };
+      };
+
+    };
+}
