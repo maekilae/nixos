4c80d457327a8b8895d3be05375b88f2d3628cd1
Maekilae <marcus@emph.dev>
2026-03-09 17:14:23 +0100

    Add readme

--- modules/nix/flake-parts/nix-darwin.nix
+++ modules/nix/flake-parts/nix-darwin.nix
@@ -5,11 +5,11 @@
 }:
 {
   # currently, there's no nix-darwin module for flake-parts,
-  # so we have to manually add flake.darwinConfigurations
+  # so we have to manually add flake.nix-darwin
 
   options = {
     flake = flake-parts-lib.mkSubmoduleOptions {
-      darwinConfigurations = lib.mkOption {
+      nix-darwin = lib.mkOption {
         type = lib.types.lazyAttrsOf lib.types.raw;
         default = { };
       };
