23c7eaf652fae688f4af6107bca241883d62dd3d
Maekilae <marcus@emph.dev>
2026-07-20 21:57:53 +0200

    Add kwallet NixOS module

---
+++ modules/services/kwallet.nix
@@ -0,0 +1,15 @@
+{
+  flake.modules.nixos.kwallet =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = with pkgs.kdePackages; [
+        kwallet
+        kwallet-pam
+        kwalletmanager
+      ];
+
+      # Auto-unlock KWallet on login. Extend to other PAM services (greetd,
+      # sddm, gdm, sudo, …) as needed for the host's display/login manager.
+      security.pam.services.login.kwallet.enable = true;
+    };
+}
